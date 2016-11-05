5eb7056e9088ca678f3b33b1e84dcb68681c785f
cashmere53 <ict.nurburgring.mikurusaders53@gmail.com>
2016-11-05 21:08:09 +0900

    a

---
+++ index.js
@@ -0,0 +1,9 @@
+/// <reference path="typings/tsd.d.ts" />
+
+var express = require('express')
+var app = express()
+
+/* 2. listen()メソッドを実行して3000番ポートで待ち受け。*/
+var server = app.listen(3000, function(){
+    console.log("Node.js is listening to PORT:" + server.address().port)
+})
