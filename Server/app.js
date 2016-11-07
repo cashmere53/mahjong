5badcbdef1244b2fe9c0b9d08fb6a0135ee441d0
cashmere53 <ict.nurburgring.mikurusaders53@gmail.com>
2016-11-08 01:03:58 +0900

    delete gulp-jade

--- Server/app.js
+++ Server/app.js
@@ -1,14 +1,8 @@
-var fs = require('fs')
 var ws = require('websocket.io')
-var http = require('http')createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'})
-    var output = fs.readFileSync('../html/index.html', 'utf-8')
-    res.end(output)
-}).listen(3000)
+var http = require('http').createServer().listen(3000)
 var server = ws.attach(http)
 
-server.on('connection', function (socket) {
-    socket.on('message', function (data) {
-        
-    })
+server.on('connection', function (client) {
+  client.on('message', function () { })
+  client.on('close', function () { })
 })
