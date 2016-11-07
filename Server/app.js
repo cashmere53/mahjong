d5ec6abc87a294379f7a01c72c74f756629af8b0
cashmere53 <ict.nurburgring.mikurusaders53@gmail.com>
2016-11-07 19:32:35 +0900

    a

--- Server/app.js
+++ Server/app.js
@@ -1,1 +1,14 @@
-var http = require('http')
+var fs = require('fs')
+var ws = require('websocket.io')
+var http = require('http')createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    var output = fs.readFileSync('../html/index.html', 'utf-8')
+    res.end(output)
+}).listen(3000)
+var server = ws.attach(http)
+
+server.on('connection', function (socket) {
+    socket.on('message', function (data) {
+        
+    })
+})
