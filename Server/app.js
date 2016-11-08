acaf71e102ffa111e4db99364bfd54bd173b7b87
cashmere53 <ict.nurburgring.mikurusaders53@gmail.com>
2016-11-09 01:32:20 +0900

    httpサーバ 条件分岐

--- Server/app.js
+++ Server/app.js
@@ -1,12 +1,59 @@
 var http = require('http')
 var socket = require('socket.io')
 var fs = require('fs')
-var settings = require('./portSettings')
 
-var server = http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'})
-    res.end(fs.readFileSync('html/index.html'))
-}).listen(settings.port, settings.host)
+var settings = require('./portSettings')
+console.log(settings)
+
+var dir = __dirname + '/../'
+var server = http.createServer()
+server.on('request', (req, res) => {
+    console.log(req.url)
+    switch (req.url) {
+    case '/style':
+        fs.readFile(dir + 'html/style.css', 'utf-8', (err, data) => {
+            if (err) {
+                res.writeHead(404, {'Content-Type': 'text/plane'})
+                res.write('Not Found')
+                return res.end
+            }
+
+            res.writeHead(200, {'Content-Type': 'text/css'})
+            res.write(data)
+            res.end()
+        })
+        break
+
+    case '/jquery':
+        fs.readFile(dir + 'bower_components/jquery/dist/jquery.min.js', 'utf-8', (err, data) => {
+            if (err) {
+                res.writeHead(404, {'Content-Type': 'text/plane'})
+                res.write('Not Found')
+                return res.end
+            }
+
+            res.writeHead(200, {'Content-Type': 'text/javascript'})
+            res.write(data)
+            res.end()
+        })
+        break
+
+    default:
+        fs.readFile(dir + 'html/index.html', 'utf-8', (err, data) => {
+            if (err) {
+                res.writeHead(404, {'Content-Type': 'text/plane'})
+                res.write('Not Found')
+                return res.end
+            }
+
+            res.writeHead(200, {'Content-Type': 'text/html'})
+            res.write(data)
+            res.end()
+        })
+        break
+    }
+})
+server.listen(settings.port, settings.host)
 
 var io = socket.listen(server)
 io.sockets.on('connection', function (client) {
