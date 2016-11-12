1f121adeb668e496c46faed0478bcca57230c2bf
cashmere53 <ict.nurburgring.mikurusaders53@gmail.com>
2016-11-12 10:14:08 +0900

    iroiro

--- Server/app.js
+++ Server/app.js
@@ -120,11 +120,13 @@ server.on('request', (req, res) => {
 })
 
 var usrHash = {}
+var score = {'p1': 25000, 'p2': 25000, 'p3': 25000, 'p4': 25000}
 io.on('connection', (client) => {
     client.on('connected', (name) => {
         var msg = name + 'さんが入室しました。'
         usrHash[client.id] = name
         io.emit('publish', {value: msg})
+        io.emit('requestScore', score)
         // console.log(name + 'さんが入室しました。')
     })
 
@@ -132,6 +134,60 @@ io.on('connection', (client) => {
         io.emit('publish', {value: data.value})
     })
     
+    client.on('sendScore', (data) => {
+        /*
+        data type:
+        親ツモ -> score <player> <score>
+            ex. score p1 4000 (親のp1が4000オールのツモ和了り)
+        ロン -> score <from> <to> <score>
+            ex. score p2 p3 7700 (p2がp3からの出和了り7700点)
+        子ツモ -> score <player> <parent> <pScore> <cScore>
+            ex. score p1 p4 4000 2000 (子のp1が([p2, p3]2000, [p4]4000)のツモ和了り)
+        */
+        var splited = data.value.split(' ')
+        if(splited.length === 3) {
+            var player = splited[1]
+            var minusScore = Number(splited[2])
+            for(var p in score) {
+                if (p !== player) {
+                    score[p] -= minusScore
+                } else {
+                    score[p] += minusScore * 3
+                }
+            }
+        } else if (splited.length === 4) {
+            var fm = splited[2]
+            var to = splited[1]
+            var moveScore = Number(splited[3])
+            score[fm] -= moveScore
+            score[to] += moveScore
+        } else if (splited.length === 5) {
+            var win = splited[1]
+            var par = splited[2]
+            var movePScore = Number(splited[3])
+            var moveCScore = Number(splited[4])
+            for(var m in score) {
+                switch(m){
+                case win:
+                    score[win] += movePScore + moveCScore * 2
+                    break
+                case par:
+                    score[par] -= movePScore
+                    break
+                default:
+                    score[m] -= moveCScore
+                    break
+                }
+            }
+        }
+        
+        io.emit('recievScore', score)
+    })
+
+    client.on('requestScore', () => {
+        io.emit('requestScore', score)
+    })
+
     client.on('disconnect', () => {
         if (usrHash[client.id]) {
             var msg = usrHash[client.id] + 'さんが退出しました。'
--- html/script.js
+++ html/script.js
@@ -1,8 +1,15 @@
-var socketio = io.connect('http://192.168.0.81:3000')
+var socketio = io.connect('http://localhost:3000')
 
 socketio.on('connected', function (name) {})
 socketio.on('publish', function (data) {
     addMessage(data.value)
+})
+socketio.on('recievScore', function (data) {
+    console.log(data)
+    rewriteScore(data)
+})
+socketio.on('requestScore', function (data) {
+    rewriteScore(data)
 })
 socketio.on('disconnect', function () {})
 
@@ -11,21 +18,72 @@ var start = function (name) {
     socketio.emit('connected', name)
 }
 
+
+var regex = new RegExp(/^score/)
 var publishMessage = function () {
     // var textInput = document.getElementById('textbox')
     var textInput = $('#textbox')
+    if(regex.test(textInput.val())) {
+        // console.log('ok')
+        socketio.emit('sendScore', {value: textInput.val()})
+    }
     var msg = '[' + myName + '] ' + textInput.val()
     socketio.emit('publish', {value: msg})
     // textInput.value = ''
     textInput.val('')
 }
 
+var tradeScore = function () {
+    
+}
+
+var messageList = []
 var addMessage = function (msg) {
     // var domMsg = document.createElement('div')
     // domMsg.innerHTML = new Date().toLocaleTimeString() + ' ' + msg
     // msgArea.appndChiled(domMsg)
-    var domMsg = $('<div>').html(new Date().toLocaleTimeString() + ' ' + msg)
-    $('#chatHistory').append(domMsg)
+    var maxConts = 10
+    messageList.push(msg)
+    $('#chatHistory').empty()
+    if (messageList.length <= maxConts) {
+        for (var i = 0; i < messageList.length; i++) {
+            var domMsg = $('<div>').html(new Date().toLocaleTimeString() + ' ' + messageList[i])
+            $('#chatHistory').append(domMsg)
+        }
+    } else {
+        messageList.shift()
+        for (var i = 0; i < maxConts; i++) {
+            var domMsg = $('<div>').html(new Date().toLocaleTimeString() + ' ' + messageList[i])
+            $('#chatHistory').append(domMsg)
+        }
+    }
+}
+
+var rewriteScore = function (data) {
+    var players = [ {player: 'p1', score: data['p1']},
+                    {player: 'p2', score: data['p2']},
+                    {player: 'p3', score: data['p3']},
+                    {player: 'p4', score: data['p4']} ]
+    players.sort(function (a, b) {
+        if (a.score > b.score) {
+            return -1
+        }
+        if (a.score < b.score) {
+            return 1
+        }
+        return 0
+    })
+    var max = Math.max.apply(null, players.map((item) => {return item.score}))
+    $('#scoreTableBody').empty()
+    for(var i in players) {
+        var str = '<tr>'
+        str += `<td>${players[i].player}</td>`
+        str += `<td>${players[i].score}</td>`
+        str += `<td>${players[i].score - max}</td>`
+        str += '</tr>'
+        $('#scoreTableBody').append(str)
+    }
+    
 }
 
 // var msgArea = document.getElementById('chatHistory')
